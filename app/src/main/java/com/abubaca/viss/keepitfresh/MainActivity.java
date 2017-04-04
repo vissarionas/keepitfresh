2f7bc43c88c5a9e11716eeeadf157eda6b596426
vissarionas <vissarionas@gmail.com>
2017-04-04 11:58:31 +0300

    added product spinner

--- app/src/main/java/com/abubaca/viss/keepitfresh/MainActivity.java
+++ app/src/main/java/com/abubaca/viss/keepitfresh/MainActivity.java
@@ -3,29 +3,17 @@ package com.abubaca.viss.keepitfresh;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static String TAG = "MAIN_ACTIVITY";
 
-    Button newCategoryBtn , submitBtn;
-    EditText input1 ,input2 ,input3 , input4;
+    Button newEntryBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,69 +24,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        newCategoryBtn = (Button)findViewById(R.id.new_category_btn);
-        submitBtn = (Button)findViewById(R.id.submit_btn);
-        input1 = (EditText)findViewById(R.id.input1);
-        input2 = (EditText)findViewById(R.id.input2);
-        input3 = (EditText)findViewById(R.id.input3);
-        input4 = (EditText)findViewById(R.id.input4);
-
-        final LinkedList<Integer> queue = new LinkedList<>();
-
-        submitBtn.setOnClickListener(new View.OnClickListener() {
-            Integer int1 , int2 , int3 , int4;
+        newEntryBtn = (Button)findViewById(R.id.new_entry_btn);
+        newEntryBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!input1.getText().toString().equals("")) {
-                    try {
-                        int1 = Integer.parseInt(input1.getText().toString());
-                    } catch (NumberFormatException e) {
-                        e.printStackTrace();
-                    }
-                    insertToQueue(queue , int1);
-                }
-                if(!input1.getText().toString().equals("")) {
-                    try {
-                        int2 = Integer.parseInt(input2.getText().toString());
-                    } catch (NumberFormatException e) {
-                        e.printStackTrace();
-                    }
-                    insertToQueue(queue , int2);
-                }
-                if(!input1.getText().toString().equals("")) {
-                    try {
-                        int3 = Integer.parseInt(input3.getText().toString());
-                    } catch (NumberFormatException e) {
-                        e.printStackTrace();
-                    }
-                    insertToQueue(queue , int3);
-                }
-                if(!input1.getText().toString().equals("")) {
-                    try {
-                        int4 = Integer.parseInt(input4.getText().toString());
-                    } catch (NumberFormatException e) {
-                        e.printStackTrace();
-                    }
-                    insertToQueue(queue , int4);
-                }
-
-                Log.i(TAG , queue.toString());
-            }
-        });
-
-
-
-
-        newCategoryBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext() , NewCategoryActivity.class));
+                startActivity(new Intent(getApplicationContext(), NewEntry.class));
             }
         });
     }
 
-    private void insertToQueue(LinkedList<Integer> queue , Integer intNum){
-        if(queue.size()>=4) queue.removeLast();
-        queue.addFirst(intNum);
-    }
 }
--- app/src/main/java/com/abubaca/viss/keepitfresh/NewEntry.java
+++ app/src/main/java/com/abubaca/viss/keepitfresh/NewEntry.java
@@ -13,35 +13,47 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
-public class NewCategoryActivity extends AppCompatActivity {
+public class NewEntry extends AppCompatActivity {
 
     private static final String TAG = "NEW_CATEGORY_ACTIVITY";
     DatabaseReference databaseReference;
     Button addCategoryBtn;
-    Spinner categorySpinner;
+    Spinner categorySpinner , productSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_category);
+        setContentView(R.layout.activity_entry);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         categorySpinner = (Spinner)findViewById(R.id.category_spinner);
+        productSpinner = (Spinner)findViewById(R.id.product_spinner);
         addCategoryBtn = (Button)findViewById(R.id.add_category_btn);
         databaseReference = FirebaseDatabase.getInstance().getReference();
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+            public void onDataChange(final DataSnapshot dataSnapshot) {
                 List<String> categories = getChildren(dataSnapshot , "");
-                SpinnerAdapter spinnerAdapter = new SpinnerAdapter(getApplicationContext() , categories);
-//                ArrayAdapter spinnerAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_spinner_item , categories);
-                categorySpinner.setAdapter(spinnerAdapter);
+                final SpinnerAdapter categoryAdapter = new SpinnerAdapter(getApplicationContext() , categories);
+                categorySpinner.setAdapter(categoryAdapter);
+                categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        List<String> codes = getChildren(dataSnapshot , categoryAdapter.getItem(position).toString());
+                        SpinnerAdapter productAdapter = new SpinnerAdapter(getApplicationContext() , codes);
+                        productSpinner.setAdapter(productAdapter);
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+                });
             }
 
             @Override
@@ -52,18 +64,9 @@ public class NewCategoryActivity extends AppCompatActivity {
         addCategoryBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new PopupDialog(NewCategoryActivity.this).addCategory();
+                new PopupDialog(NewEntry.this).addCategory();
             }
         });
-    }
-
-//    Logs a List item with the children under the parent String parameter(used in onDataChange())
-    private void logChildren(DataSnapshot dataSnapshot , String parent){
-        List<String> children = new ArrayList<>();
-        for(DataSnapshot snapshot : dataSnapshot.child(parent).getChildren()){
-            children.add(snapshot.getKey());
-        }
-        Log.i(TAG , children.toString());
     }
 
     //    Outputs a List item with the children under the parent String parameter(used in onDataChange())
